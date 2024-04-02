fc23a79a42d000092edc67bb3f844b4f301b1fce
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-04-02 16:40:31 -0500

    added idb and manifest

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
+// Imports necessary files
 import { openDB } from 'idb';
 
+// Creates function to initialize database and object store
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,10 +14,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Adds logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+console.log("You are POSTing to the database.");
+const jateDb = await openDB("jate", 1);
+const transaction = jateDb.transaction("jate", "readwrite");
+const store = transaction.objectStore("jate");
+const request = store.add( { jate: content });
+const result = await request;
+console.log("Your data has been saved to the database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Adds logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GETting all from the database");
+  const jateDb = await openDB("jate", 1);
+  const transaction = jateDb.transaction("todos", "readonly");
+  const store = transaction.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("Data retrieved", result);
+  return result;
+}
 
 initdb();
